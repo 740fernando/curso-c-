43de305698a53a9c71731231a131b3c6e2c24e23
Fernando <fernando.luiz92@hotmail.com>
2021-11-15 20:41:11 -0300

    adicionado nova branch para estudos sobre interfaces

---
+++ 9-Interfaces/ProjetoCarRental/ProjetoCarRental/Program.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+using System.Globalization;
+using ProjetoCarRental.entities;
+
+namespace ProjetoCarRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do aluguel ");
+            Console.WriteLine("Car model : ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy HH:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy HH:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            CarRental carRental = new CarRental(start,finish,new Vehicle(model));
+
+        }
+    }
+}
---
+++ 9-Interfaces/ProjetoCarRental/ProjetoCarRental/Services/RentalService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProjetoCarRental.entities;
+namespace ProjetoCarRental.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+        public void ProcessInvoice (CarRental carRental)
+        {
+
+        }
+    }
+}
