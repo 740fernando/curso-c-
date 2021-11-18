5e21e274792ef54a8480c474f6c7dc274a05223a
Fernando <fernando.luiz92@hotmail.com>
2021-11-17 21:56:25 -0300

    Solucao com interface

--- 9-Interfaces/ProjetoCarRental/ProjetoCarRental/Services/RentalService.cs
+++ 9-Interfaces/ProjetoCarRental/ProjetoCarRental/Services/RentalService.cs
@@ -6,12 +6,13 @@ namespace ProjetoCarRental.Services
     {
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
-        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+        private ITaxService _taxService;
 
-        public RentalService(double pricePerHour, double pricePerDay)
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
         {
             PricePerHour = pricePerHour;
             PricePerDay = pricePerDay;
+            _taxService = taxService; // inversao de controle por meio de injecao de depedencia.
         }
         public void ProcessInvoice (CarRental carRental)
         {
@@ -25,7 +26,7 @@ namespace ProjetoCarRental.Services
             {
                 basicPayment = PricePerDay *Math.Ceiling(duration.TotalDays);
             }
-            double tax = _brazilTaxService.Tax(basicPayment);
+            double tax = _taxService.Tax(basicPayment);
 
             carRental.Invoice= new Invoice(basicPayment,tax);
         }
